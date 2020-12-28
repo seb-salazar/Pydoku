7c4a3897abe1d7e36ec9e8c48a2512d828687ce4
Tomás Peña <tomasmenosmal@MacBook-Pro-de-Tomas.local>
2020-12-27 21:47:18 -0300

    functional commit with new solve button, still buggy

--- pydoku.py
+++ pydoku.py
@@ -13,67 +13,90 @@ if __name__ == '__main__':
     import tkinter as tk
     from tkinter import messagebox
     import numpy as np
+    import itertools
 
-    master = tk.Tk()
-    master.title("Pydoku")
-
-    w = tk.Canvas(master, width=404, height=444)
-    w.pack()
-
-    def thin_line(x1, y1, x2, y2):
-        w.create_line(x1, y1, x2, y2, fill="black")
-
-    def wide_line(x1, y1, x2, y2):
-        w.create_line(x1, y1, x2, y2, fill="black", width= 3)
-
-    #Draw thin lines
-    #draw the verticals
-    for x in range(52, 380, 44):
-        thin_line(x,10,x,404)
-
-    #draw the horizontals
-    for y in range(52, 380, 44):
-        thin_line(10,y,400,y)
-
-    #Draw wide lines
-    #draw the verticals
-    for x in range(120 + 20, 360 + 20, 120 + 12):
-        wide_line(x,10,x,404)
-
-    #draw the horizontals
-    for y in range(120 + 20, 360 + 20, 120 + 12):
-        wide_line(10,y,400,y)
-
-
-    #Now the entry boxes
     entries = []
-    for a in range(30, 130*3, 44):
-        for b in range(30, 130*3, 44):
-            large_font = ('Verdana',24)
-
-            entry1Var = tk.StringVar()
-            temp = tk.Entry(master,textvariable=entry1Var,font=large_font, width=1, borderwidth = 0, highlightthickness = 0)
-            w.create_window(b, a, window=temp)
-            #b and a are swapped to follow the solver logic
-            entries.append(temp)
-
-    #Text and button
-    Label_pydoku = tk.Label(master, text="Pydoku", font=('Helvetica', 22, 'bold'))
-    w.create_window(52, 425, window=Label_pydoku)
-
-    Label_seb = tk.Label(master, text="by Seb Salazar", font=('Helvetica', 14))
-    w.create_window(150, 427, window=Label_seb)
+    grid_to_colour = []
 
 
-    def showMsg():
-        messagebox.showinfo("Error", "Input a number, from 1 to 9")
+    def input_window():
+        global entries
+        
+        master = tk.Tk()
+        master.title("Input")
+        master.attributes("-topmost", True)     # for the window to be in top of others
+
+        w = tk.Canvas(master, width=404, height=444)
+        w.pack()
+
+        def thin_line(x1, y1, x2, y2):
+            w.create_line(x1, y1, x2, y2, fill="black")
+
+        def wide_line(x1, y1, x2, y2):
+            w.create_line(x1, y1, x2, y2, fill="black", width= 3)
+
+        #Draw thin lines
+        #draw the verticals
+        for x in range(52, 380, 44):
+            thin_line(x,10,x,404)
+
+        #draw the horizontals
+        for y in range(52, 380, 44):
+            thin_line(10,y,400,y)
+
+        #Draw wide lines
+        #draw the verticals
+        for x in range(120 + 20, 360 + 20, 120 + 12):
+            wide_line(x,10,x,404)
+
+        #draw the horizontals
+        for y in range(120 + 20, 360 + 20, 120 + 12):
+            wide_line(10,y,400,y)
 
 
-    
+        #Now the entry boxes
+        for a in range(30, 130*3, 44):
+            for b in range(30, 130*3, 44):
+                large_font = ('Verdana',24)
+
+                entry1Var = tk.StringVar()
+                temp = tk.Entry(master,textvariable=entry1Var,font=large_font, width=1, borderwidth = 0, highlightthickness = 0)
+                w.create_window(b, a, window=temp)
+                #b and a are swapped to follow the solver logic
+                entries.append(temp)
+
+        #Text and button
+        Label_pydoku = tk.Label(master, text="Pydoku", font=('Helvetica', 22, 'bold'))
+        w.create_window(52, 425, window=Label_pydoku)
+
+        Label_seb = tk.Label(master, text="by Seb Salazar", font=('Helvetica', 14))
+        w.create_window(150, 427, window=Label_seb)
+
+        def buttonAction():
+            #global master
+            getInputs()
+            master.destroy()
+
+            solver()
+
+            output_window()
+
+        solveButton = tk.Button(master, text = "Solve", command = buttonAction)
+        solveButton.configure(width = 10, activebackground = "#33B5E5")
+        solveButton_window = w.create_window(300, 427, window=solveButton)
+
+
+        master.mainloop()
+
+
+
+    # get inputs and prepare the solution
+
     grid_output = []
 
     def getInputs():
         global grid_output
+        global grid_to_colour
         wrongChecker = 0
 
         if all(elements.get() == "" for elements in entries):
@@ -100,6 +123,18 @@ if __name__ == '__main__':
                 grid_output.append(list(row_new))
 
 
+
+        # To colour the answer
+        flatten = itertools.chain.from_iterable
+        grid_to_colour = tuple(flatten(grid_output[:]))
+
+
+
+
+    def showMsg():
+        messagebox.showinfo("Error", "Input a number, from 1 to 9")
+
+    
     '''
     #"The hardest one there is" (according to Google Search)
     grid_output = [[0,0,5,3,0,0,0,0,0],
@@ -130,7 +165,10 @@ if __name__ == '__main__':
                     return False
         return True
 
+
+
     def solver():
+        #new_window()
         global grid_output
         for y in range(9):
             for x in range(9):
@@ -143,17 +181,101 @@ if __name__ == '__main__':
                             
                     return
         print(np.matrix(grid_output))
-        #input("Wanna check if there are more solutions?")
+        output_window()
+        #input("Want more")
+        #raw_input("Want more")
+        
 
-    def buttonAction():
-        getInputs()
-        solver()
+    #window to display the solution
+
     
+    def output_window():
+        global master
+        global solution
+        global entries
+        global grid_to_colour
+
+        flatten = itertools.chain.from_iterable
+        solution = list(flatten(grid_output))
 
 
-    solveButton = tk.Button(master, text = "Solve", command = buttonAction)
-    solveButton.configure(width = 10, activebackground = "#33B5E5")
-    solveButton_window = w.create_window(300, 427, window=solveButton)
+        master = tk.Tk()
+        master.title("Solution")
+        master.attributes("-topmost", True)    
+
+        w = tk.Canvas(master, width=404, height=444)
+        w.pack()
+
+        def thin_line(x1, y1, x2, y2):
+            w.create_line(x1, y1, x2, y2, fill="black")
+
+        def wide_line(x1, y1, x2, y2):
+            w.create_line(x1, y1, x2, y2, fill="black", width= 3)
+
+        #Draw thin lines
+        #draw the verticals
+        for x in range(52, 380, 44):
+            thin_line(x,10,x,404)
+
+        #draw the horizontals
+        for y in range(52, 380, 44):
+            thin_line(10,y,400,y)
+
+        #Draw wide lines
+        #draw the verticals
+        for x in range(120 + 20, 360 + 20, 120 + 12):
+            wide_line(x,10,x,404)
+
+        #draw the horizontals
+        for y in range(120 + 20, 360 + 20, 120 + 12):
+            wide_line(10,y,400,y)
+
+        labels = []
+
+        #Now the display boxes
+        for a in range(30, 130*3, 44):
+            for b in range(30, 130*3, 44):
+                large_font = ('Verdana',24)
+
+               
+                temp = tk.Label(master,font=large_font, width=1, borderwidth = 0, highlightthickness = 0)
+                w.create_window(b, a, window=temp)
+                #b and a are swapped to follow the solver logic
+                labels.append(temp)
+
+        #Text and button
+        Label_pydoku = tk.Label(master, text="Pydoku", font=('Helvetica', 22, 'bold'))
+        w.create_window(52, 425, window=Label_pydoku)
+
+        Label_seb = tk.Label(master, text="by Seb Salazar", font=('Helvetica', 14))
+        w.create_window(150, 427, window=Label_seb)
+
+        def buttonAction():
+            global entries
+            #global master
+
+            master.destroy()
+            input_window()
+            entries = []
+         #   getInputs()
+          #  solver()
+
+        for i in range(81):
+            labels[i]['text'] = str(solution[i])
+            if grid_to_colour[i] == 0:
+                labels[i]['text'] = str(solution[i])
+                labels[i].config(fg="red")
+            
+                
 
 
-    master.mainloop()
+
+        solveAnotherButton = tk.Button(master, text = "Solve Another", command = buttonAction)
+        solveAnotherButton.configure(width = 10, activebackground = "#33B5E5")
+        solveAnotherButton_window = w.create_window(300, 427, window=solveAnotherButton)
+
+
+        master.mainloop()
+
+    
+    input_window()
